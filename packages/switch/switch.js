b22e367e673aaf917b5f85328fec5c7fa0c3042d
Donovan Buck <donovan@donovan.bz>
2019-09-26 20:42:06 -0500

    Switch first commit

--- packages/switch/switch.js
+++ packages/switch/switch.js
@@ -1,6 +1,6 @@
 import { Emitter } from "@embedded/event"
 
-**
+/*
  * Switch
  * @constructor
  *
@@ -11,7 +11,9 @@ import { Emitter } from "@embedded/event"
 class Switch extends Emitter {
   
   #state = {
-    isOpen: true
+    isOpen: true,
+    normallyOpen: true,
+    raw: null 
   };
   
   constructor(opts) {    
@@ -20,7 +22,8 @@ class Switch extends Emitter {
     
     this.io = opts.io;
     
-    
+    // Is this instance Normally Open
+    this.#state.normallyOpen = opts.type !== "NC";
 
     Object.defineProperties(this, {
       isClosed: {
@@ -36,6 +39,143 @@ class Switch extends Emitter {
     });
 
   }
+
+
+  /*
+
+  // Create a 5 ms debounce boundary on event triggers
+  // this avoids button events firing on
+  // press noise and false positives
+  var trigger = Fn.debounce(function(key) {
+    aliases[key].forEach(function(type) {
+      this.emit(type, null);
+    }, this);
+  }, 5);
+  
+  
+
+  // Logical Defaults
+  var closeValue = 1;
+  var openValue = 0;
+
+  if (invert) {
+    closeValue ^= 1;
+    openValue ^= 1;
+  }
+
+  this.io.pinMode(this.pin, this.io.MODES.INPUT);
+
+  if (isNormallyOpen) {
+    this.io.digitalWrite(this.pin, this.io.HIGH);
+  }
+
+  this.io.digitalRead(this.pin, function(data) {
+    raw = data;
+
+    trigger.call(this, this.isOpen ? "open" : "close");
+  }.bind(this));
+
+  Object.defineProperties(this, {
+    value: {
+      get: function() {
+        return Number(this.isOpen);
+      }
+    },
+    invert: {
+      get: function() {
+        return invert;
+      },
+      set: function(value) {
+        invert = value;
+        closeValue = invert ? 0 : 1;
+        openValue = invert ? 1 : 0;
+      }
+    },
+    closeValue: {
+      get: function() {
+        return closeValue;
+      },
+      set: function(value) {
+        closeValue = value;
+        openValue = value ^ 1;
+      }
+    },
+    openValue: {
+      get: function() {
+        return openValue;
+      },
+      set: function(value) {
+        openValue = value;
+        closeValue = value ^ 1;
+      }
+    },
+    isOpen: {
+      get: function() {
+        return raw === openValue;
+      }
+    },
+    isClosed: {
+      get: function() {
+        return raw === closeValue;
+      }
+    },
+  });
+}
+
+util.inherits(Switch, Emitter);
+
+
+/**
+ * Fired when the Switch is close
+ *
+ * @event
+ * @name close
+ * @memberOf Switch
+ *
+
+
+/**
+ * Fired when the Switch is opened
+ *
+ * @event
+ * @name open
+ * @memberOf Switch
+ *
+
+
+/**
+ * Switches()
+ * new Switches()
+ *
+ * Constructs an Array-like instance of all servos
+ *
+
+function Switches(numsOrObjects) {
+  if (!(this instanceof Switches)) {
+    return new Switches(numsOrObjects);
+  }
+
+  Object.defineProperty(this, "type", {
+    value: Switch
+  });
+
+  Collection.Emitter.call(this, numsOrObjects);
+}
+
+util.inherits(Switches, Collection.Emitter);
+
+Collection.installMethodForwarding(
+  Switches.prototype, Switch.prototype
+);
+
+// Assign Switches Collection class as static "method" of Switch.
+Switch.Collection = Switches;
+
+
+
+module.exports = Switch;
+
+*/
 
 };
 
