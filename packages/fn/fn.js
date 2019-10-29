126bc9315ba37a41e2a104816193c28565d29371
Donovan Buck <donovan@donovan.bz>
2019-10-29 15:40:46 -0400

    Update fn to add timer methods
    Still doesn't work

--- packages/fn/fn.js
+++ packages/fn/fn.js
@@ -1,14 +1,41 @@
 const normalizeParams = function(io, pin) {
   
-  let deviceOpts = pin;
+  let deviceOpts = {};
   let ioOpts = io;
   
   if (typeof io === "function") ioOpts = { io };
-  if (typeof pin === "undefined") deviceOpts = {};
   if (typeof pin === "number" || typeof pin === "string") ioOpts.pin = pin;
+
+  if (typeof pin === "undefined") deviceOpts = {};
+  if (typeof pin === "object") deviceOpts = pin;
 
   return { ioOpts, deviceOpts}
 };
 
-export { normalizeParams };
+const constrain = function(value, low, high) {
+  if (value > high) value = high;
+  if (value < low) value = low;
+  return value;
+}
+
+const setInterval = function() {
+  if (typeof System !== "undefined") {
+    return System.setInterval;
+  }
+  return GLOBAL.setInterval;
+}
+
+const clearInterval = function() {
+  if (typeof System !== "undefined") {
+    return System.clearInterval;
+  }
+  return GLOBAL.clearInterval;
+}
+
+export { 
+  clearInterval,
+  constrain,
+  normalizeParams,
+  setInterval
+};
 
--- packages/fn/test/test.js
+++ packages/fn/test/test.js
@@ -3,22 +3,81 @@ const assert = require('assert');
 const sinon = require('sinon');
 
 import { Digital } from './../../mock/provider.js';
-import Fn from './../fn.js';
+import { clearInterval, constrain, normalizeParams, setInterval } from './../fn.js';
 
 describe('Fn', function() {
   
-  describe('Normalize', function() {
+  describe('normalizeParams', function() {
     
     it('should return valid ioOpts and deviceOpts when passed a provider class and pin number', function() {
-      
       const {ioOpts, deviceOpts} = normalizeParams(Digital, 13);
-      assert.equal(led instanceof Led, true);
-      assert.equal(led.io instanceof Digital, true);
-      assert.equal(led.LOW, 0);
-      assert.equal(led.HIGH, 1);
-    
+      assert.equal(Object.is(ioOpts.io, Digital), true);
+      assert.equal(ioOpts.pin, 13);
+      assert.equal(typeof deviceOpts, "object");
+      assert.equal(Object.keys(deviceOpts).length, 0);
+    });
+
+    it('should return valid ioOpts and deviceOpts when passed a provider object', function() {
+      const {ioOpts, deviceOpts} = normalizeParams({
+        io: Digital,
+        pin: 13
+      });
+      assert.equal(Object.is(ioOpts.io, Digital), true);
+      assert.equal(ioOpts.pin, 13);
+      assert.equal(typeof deviceOpts, "object");
+      assert.equal(Object.keys(deviceOpts).length, 0);
+    });
+
+    it('should return valid ioOpts and deviceOpts when passed a provider object and device object', function() {
+      const {ioOpts, deviceOpts} = normalizeParams({
+        io: Digital,
+        pin: 13
+      }, {
+        someProp: 1
+      });
+      assert.equal(Object.is(ioOpts.io, Digital), true);
+      assert.equal(ioOpts.pin, 13);
+      assert.equal(typeof deviceOpts, "object");
+      assert.equal(Object.keys(deviceOpts).length, 1);
+      assert.equal(Object.keys(deviceOpts)[0], "someProp");
     });
 
   });
+
+  describe('constrain', function() {
+    
+    it('should return the value when value is in range', function() {
+      const result = constrain(128, 0, 255);
+      assert.equal(result, 128);
+    });
+
+    it('should return the max when value is beyond range', function() {
+      const result = constrain(512, 0, 255);
+      assert.equal(result, 255);
+    });
+
+    it('should return the min when value is below range', function() {
+      const result = constrain(1, 128, 255);
+      assert.equal(result, 128);
+    });
+
+    it('should work with a negative min', function() {
+      const result = constrain(128, -128, 255);
+      assert.equal(result, 128);
+    });
+
+    it('should work with all negative numbers', function() {
+      const result = constrain(-128, -255, -0);
+      assert.equal(result, -128);
+    });
+
+  });
+
+  // describe('setInterval', function() {
+  //   it('should return the GLOBAL setInterval by default', function() {
+  //     const localSetInterval = setInterval;
+  //     assert.equal(Object.is(localSetInterval, GLOBAL.setInterval), true);
+  //   });
+  // });
 
 });
