d4bf65d824f250ae8738aca0b5e65229d03e7d15
Donovan Buck <donovan@donovan.bz>
2020-06-14 18:59:23 -0500

    Regenerate docs and dist file

--- packages/isl29125/examples/rgb-mimicer/main.js
+++
@@ -1,6 +0,0 @@
-import ISL29125 from "@j5e/isl29125";
-
-const color = await new ISL29125({
-	sda: 4,
-	scl: 5
-});
--- packages/tcs34725/examples/rgb-mimic/main.js
+++
@@ -1,6 +0,0 @@
-import TCS34725 from "@j5e/tcs34725";
-
-const color = await new TCS34725({
-	sda: 4,
-	scl: 5
-});
